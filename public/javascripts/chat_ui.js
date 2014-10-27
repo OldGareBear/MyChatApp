16780266b961c5483689e858351c3c3970d16496
App Academy Student <student@appacademy.io>
2014-10-27 15:30:42 -0400

    get basic chat functionality working

---
+++ public/javascripts/chat_ui.js
@@ -0,0 +1,40 @@
+(function() {
+  if (typeof ChatApp === 'undefined') {
+    window.ChatApp = {};
+  }
+
+  var ChatUI = ChatApp.ChatUI = function(chat) {
+    this.chat = chat
+  };
+
+  ChatUI.prototype.getInput = function () {
+    return $("form.send > input").val();
+  };
+
+  ChatUI.prototype.sendMessage = function () {
+    this.chat.sendMessage(this.getInput());
+  };
+
+  ChatUI.prototype.displayMessage = function() {
+    var chatUI = this;
+
+    this.chat.socket.on("message", function(data) {
+      $("div.display").text(chatUI.getInput());
+    });
+  };
+
+  var socket = io();
+  var chat = new window.ChatApp.Chat(socket);
+  var chatUI = new ChatUI(chat);
+  chatUI.displayMessage();
+
+  $("form.send").on("submit", function(event) {
+    event.preventDefault();
+
+    console.log("Text:" + chatUI.getInput());
+
+    chatUI.sendMessage();
+    chat.sendMessage();
+  });
+
+})();
